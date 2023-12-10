bbf5503943e3e077e34968fc4799de482e578be5
Vincent Lyau <31805299+vinlyau@users.noreply.github.com>
2023-12-09 17:52:32 -0800

    Add relic name detection

--- crkrelicbot/__main__.py
+++ crkrelicbot/__main__.py
@@ -1,8 +1,9 @@
 import easyocr
 import os
-from datetime import date
+import time
+from pprint import pprint
 
-from .game_capture import capture_window, crop_to_donors
+from .game_capture import capture_window, crop_to_donors, crop_to_relic_name
 from .image_utils import process_image_for_ocr
 
 
@@ -12,29 +13,31 @@ IMAGES_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images')
 reader = easyocr.Reader(['en'])
 
 
-def setup_images_dir():
-    if not os.path.isdir(IMAGES_DIR):
-        os.makedirs(IMAGES_DIR)
-
-
 def process_one_capture(img_dir: str):
     # Take one screen capture
     full_filepath = os.path.join(img_dir, 'full.png')
     capture_window(CRK_WINDOW_NAME, full_filepath)
 
-    # Crop out everything besides the donors list
+    # Crop out relic name and donors list
+    relic_name_filepath = crop_to_relic_name(full_filepath)
     donors_filepath = crop_to_donors(full_filepath)
 
-    # Pre-process image to improve OCR quality
+    # Pre-process images to improve OCR quality
+    process_image_for_ocr(relic_name_filepath, invert=True, threshold=150, dilate=False)
     process_image_for_ocr(donors_filepath)
 
-    # Run OCR on processed image
-    text = reader.readtext(donors_filepath, detail=0)
-    print(text)
+    # Run OCR on processed images
+    # TODO: pprints to be removed
+    relic_name = reader.readtext(relic_name_filepath, detail=0)
+    pprint(relic_name)
+    donors_list = reader.readtext(donors_filepath, detail=0)
+    pprint(donors_list)
+
+    # TODO: Add extracted info to sheet/other data storage
 
 
 def run():
-    run_img_dir = os.path.join(IMAGES_DIR, str(date.today()))
+    run_img_dir = os.path.join(IMAGES_DIR, time.strftime("%Y_%m_%d_%H_%M_%S"))
     if not os.path.isdir(run_img_dir):
         os.makedirs(run_img_dir)
 
@@ -42,5 +45,4 @@ def run():
 
 
 if __name__ == '__main__':
-    setup_images_dir()
     run()
--- crkrelicbot/image_utils.py
+++ crkrelicbot/image_utils.py
@@ -3,22 +3,34 @@ import numpy as np
 from PIL import Image
 
 
-def binarize_image(img: np.ndarray) -> np.ndarray:
-    _, img_thresholded = cv2.threshold(img, 125, 255, cv2.THRESH_BINARY)
+def binarize_image(img: np.ndarray, threshold: int) -> np.ndarray:
+    _, img_thresholded = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
     return img_thresholded
 
 
 def dilate_image(img: np.ndarray) -> np.ndarray:
-    return cv2.dilate(img, cv2.getGaussianKernel(3, 1), iterations=1)
+    return cv2.dilate(img, cv2.getGaussianKernel(3, 1))
 
 
-def process_image_for_ocr(filepath: str):
+def invert_image(img: np.ndarray) -> np.ndarray:
+    return cv2.bitwise_not(img)
+
+
+def process_image_for_ocr(filepath: str,
+                          binarize: bool=True,
+                          invert: bool=False,
+                          dilate: bool=True,
+                          threshold: int=125):
     with Image.open(filepath) as img:
         img_gray = img.convert('L')
         img_arr = np.asarray(img_gray)
 
-        img_arr = binarize_image(img_arr)
-        img_arr = dilate_image(img_arr)
+        if binarize:
+            img_arr = binarize_image(img_arr, threshold)
+        if invert:
+            img_arr = invert_image(img_arr)
+        if dilate:
+            img_arr = dilate_image(img_arr)
 
         img_processed = Image.fromarray(img_arr)
         img_processed.save(filepath)
