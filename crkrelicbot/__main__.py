910b4383ab4535361020ff92bf0c8d2dbd286730
Vincent Lyau <31805299+vinlyau@users.noreply.github.com>
2023-12-11 10:12:58 -0800

    Relic Navigation (#3)

--- crkrelicbot/__main__.py
+++ crkrelicbot/__main__.py
@@ -5,28 +5,31 @@ import time
 from pprint import pprint
 from string import ascii_lowercase, ascii_uppercase, punctuation, whitespace
 
-from .game_capture import capture_window, crop_to_donors, crop_to_qtys, crop_to_relic_name
-from .image_utils import process_image_for_ocr
+from . import window
+from .image_utils import process_image_for_ocr, crop_to_donors, crop_to_qtys, crop_to_relic_name
 
 
 # -- Script-required metadata --
-CRK_WINDOW_NAME = 'CookieRun: Kingdom'
 IMAGES_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images')
 
 # -- Useful Macros --
 KNAME_BLOCKLIST = f'{punctuation}{whitespace}'
 RNAME_ALLOWLIST = f'{ascii_lowercase}{ascii_uppercase}\'-&: '
 RQTYS_ALLOWLIST = 'x0123456789'
+OCR_BBOX_IDX = 0
+OCR_TEXT_IDX = 1
+OCR_CONF_IDX = 2
 
 # -- Globals --
 # easyocr reader only needs to be initialized once per language combination
 reader = easyocr.Reader(['en'])
 
 
-def process_one_capture(img_dir: str):
+# Processes a single screenshot of a Relic Details page
+def process_one_capture(img_dir: str, first_capture: bool=False):
     # Take one screen capture
     full_filepath = os.path.join(img_dir, 'full.png')
-    capture_window(CRK_WINDOW_NAME, full_filepath)
+    window.capture(full_filepath)
 
     # Crop out relic name and donors list
     relic_name_filepath = crop_to_relic_name(full_filepath)
@@ -39,7 +42,7 @@ def process_one_capture(img_dir: str):
     process_image_for_ocr(qtys_filepath, threshold=100)
 
     # Run OCR on processed images
-    # TODO: pprints to be removed
+    # TODO: prints/pprints to be removed
     relic_name = reader.readtext(relic_name_filepath, allowlist=RNAME_ALLOWLIST, paragraph=True)
     pprint(relic_name)
     donors_list = reader.readtext(donors_filepath, blocklist=KNAME_BLOCKLIST)
@@ -47,7 +50,28 @@ def process_one_capture(img_dir: str):
     qtys_list = reader.readtext(qtys_filepath, allowlist=RQTYS_ALLOWLIST)
     pprint(qtys_list)
 
-    # TODO: Add extracted info to sheet/other data storage
+    # On first capture only, if the first QTY detected has low confidence
+    # then scroll a tiny bit down in order to skip the 'First Donor' row
+    if first_capture:
+        if qtys_list[0][OCR_CONF_IDX] < 0.95:
+            print('Most likely detected the "Donor" string in qtys list')
+            window.scroll_one_relic_row(full_filepath)
+            process_one_capture(img_dir)
+
+    return full_filepath
+
+
+# Processes multiple screenshots covering the relic's entire Donors list
+def process_relic(img_dir: str):
+    # Process the first screenshot - there are special rules applicable here
+    full_filepath = process_one_capture(img_dir, first_capture=True)
+
+    # TODO: fix below to intelligently determine when Donors list is complete
+    # Loop until entire Donors list has been processed
+    window.scroll_one_relic_page(full_filepath)
+
+    # At end of processing current relic, click to next relic
+    # window.click_to_next_relic(full_filepath) # TODO: uncomment this line
 
 
 def run():
@@ -57,7 +81,7 @@ def run():
 
     # TODO: change this to a function that will run for all relics/pages
     #       currently left as-is for testing purposes only
-    process_one_capture(run_img_dir)
+    process_relic(run_img_dir)
 
     # Duplicate the images in a 'latest' directory for convenience
     # TODO: Add ability to specify CL arg to disable this feature
@@ -66,4 +90,6 @@ def run():
 
 
 if __name__ == '__main__':
+    window.prepare()
     run()
+    window.cleanup()
---
+++ crkrelicbot/window.py
@@ -0,0 +1,112 @@
+import pyautogui
+import pywinauto
+import time
+import win32gui
+import win32ui
+from ctypes import windll
+from PIL import Image
+
+
+# -- Script-required metadata --
+CRK_WINDOW_NAME = 'CookieRun: Kingdom'
+CRK_HWND = win32gui.FindWindow(None, CRK_WINDOW_NAME)
+
+
+# -- Globals --
+crk_window = None
+
+
+def prepare():
+    global crk_window
+    crk_app = pywinauto.Application()
+    crk_app.connect(handle=CRK_HWND)
+    crk_window = crk_app.window()
+    if crk_window.is_minimized():
+        raise Exception('Game window cannot be minimized prior to script run')
+    crk_app.window().maximize()
+    crk_app.window().set_focus()
+
+
+def cleanup():
+    global crk_window
+    crk_window.minimize()
+
+
+# Adapted from https://stackoverflow.com/questions/7561049/what-is-the-
+# difference-between-getclientrect-and-getwindowrect-in-winapi
+def capture(filepath: str):
+    # Get window sizing
+    left, top, right, bot = win32gui.GetClientRect(CRK_HWND)
+    width = right - left
+    height = bot - top
+
+    hwndDC = win32gui.GetWindowDC(CRK_HWND)
+    mfcDC = win32ui.CreateDCFromHandle(hwndDC)
+    saveDC = mfcDC.CreateCompatibleDC()
+
+    saveBitMap = win32ui.CreateBitmap()
+    saveBitMap.CreateCompatibleBitmap(mfcDC, width, height)
+
+    saveDC.SelectObject(saveBitMap)
+
+    result = windll.user32.PrintWindow(CRK_HWND, saveDC.GetSafeHdc(), 3)
+    if result == 0:
+        raise Exception('Failed to PrintWindow')
+
+    bmpinfo = saveBitMap.GetInfo()
+    bmpstr = saveBitMap.GetBitmapBits(True)
+
+    win32gui.DeleteObject(saveBitMap.GetHandle())
+    saveDC.DeleteDC()
+    mfcDC.DeleteDC()
+    win32gui.ReleaseDC(CRK_HWND, hwndDC)
+
+    size = (bmpinfo['bmWidth'], bmpinfo['bmHeight'])
+    with Image.frombuffer('RGB', size, bmpstr, 'raw', 'BGRX', 0, 1) as img:
+        img.save(filepath)
+
+
+def drag(full_img: str, x1: float, y1: float, x2: float, y2: float,
+         duration: float=0.5, hold_s: float=0.5, grace_s: float=0.5):
+    with Image.open(full_img) as img:
+        width, height = img.size
+        start_x = int(width * x1)
+        start_y = int(height * y1)
+        end_x = int(width * x2)
+        end_y = int(height * y2)
+
+    # Convert from client coordinates (e.g., relative position within game window)
+    # to absolute screen coordinates for pyautogui
+    start_x, start_y = win32gui.ClientToScreen(CRK_HWND, (start_x, start_y))
+    end_x, end_y = win32gui.ClientToScreen(CRK_HWND, (end_x, end_y))
+
+    pyautogui.moveTo(x=start_x, y=start_y)
+    pyautogui.mouseDown()
+    pyautogui.moveTo(x=end_x, y=end_y, duration=duration)
+    time.sleep(hold_s) # Need to wait to nullify inertial scrolling
+    pyautogui.mouseUp()
+    time.sleep(grace_s) # Grace period since scrolling too far results in a scrollback
+
+
+def scroll_one_relic_page(full_img: str):
+    drag(full_img, .7117, .5739, .7117, .2463, duration=0.75)
+
+
+def scroll_one_relic_row(full_img: str):
+    drag(full_img, .7117, .5739, .7117, .5175, duration=0.5)
+
+
+def click(full_img: str, x: float, y: float):
+    with Image.open(full_img) as img:
+        width, height = img.size
+        click_x = int(width * x)
+        click_y = int(height * y)
+
+    # Convert from client coordinates (e.g., relative pixel position in-game)
+    # to absolute screen coordinates for pyautogui
+    click_x, click_y = win32gui.ClientToScreen(CRK_HWND, (click_x, click_y))
+    pyautogui.click(x=click_x, y=click_y, duration=0.5)
+
+
+def click_to_next_relic(full_img: str):
+    click(full_img, .4841, .4361)
